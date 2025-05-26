7aceedac3020fa8ea5a4180cf347b19d52aaf6ec
Daniel Louis <Llouis5314@gmail.com>
2025-05-26 08:45:30 -0400

    Create 13.2_ChallangeSolution.py

---
+++ Section_1/Chapter_13_ListsAdvanced/13.2_ChallangeSolution.py
@@ -0,0 +1,11 @@
+original_list = input().split(',')
+# Write your code below
+list1 = original_list[2::4]
+lsit2 = original_list[2:len(original_list)-2]
+list3 = original_list[::-2]
+list4 = original_list[-3:]
+# Don't change below this line
+print("List 1:", list1)
+print("List 2:", list2)
+print("List 3:", list3)
+print("List 4:", list4)
