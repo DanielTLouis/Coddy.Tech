c4b70e0d1dd871b2f8069da2300bb0c4d8435a04
Daniel Louis <Llouis5314@gmail.com>
2025-05-26 08:51:38 -0400

    Create 14.2_Solution.py

---
+++ Section_1/Chapter_14_DailyExpenseTracker/14.2_Solution.py
@@ -0,0 +1,14 @@
+print("Welcome to the Daily Expense Tracker!")
+print("")
+print("Menu:") 
+print("1. Add a new expense")
+print("2. View all expenses")
+print("3. Calculate total and average expense")
+print("4. Clear all expenses")
+print("5. Exit")
+loop = True
+while loop:
+    choice = int(input()) 
+    if(choice == 5):
+        loop = False 
+print("Exiting the Daily Expense Tracker. Goodbye!")
