5192cb3dd307077c50054936b27cc22b05f1ba28
Daniel Louis <Llouis5314@gmail.com>
2025-09-01 19:50:35 -0400

    Create 4.2_Challange_Solution.py

---
+++ Section_1/Chapter_04_ OperatorsPart2/4.2_Challange_Solution.py
@@ -0,0 +1,10 @@
+# Replace the values with booleans
+a = ?
+b = ?
+c = ?
+
+# This line checks if (a or b) and not c is True
+result = (a or b) and not c
+
+# Don't change the line below
+print(f"result = {result}")
