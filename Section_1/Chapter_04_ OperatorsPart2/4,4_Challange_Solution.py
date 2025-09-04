6e69ed8dcc3509c894e097b80028c751bad4712b
Daniel Louis <Llouis5314@gmail.com>
2025-09-03 21:23:57 -0400

    Create 4,4_Challange_Solution.py

---
+++ Section_1/Chapter_04_ OperatorsPart2/4,4_Challange_Solution.py
@@ -0,0 +1,15 @@
+# Initialize variables
+is_sunny = True
+temperature = 25
+wind_speed = 10
+water_temprature = 22
+
+# Calculate conditions
+can_go_hiking = is-sunny and tempatue > 15 and wind_speed < 20
+can_go_swimming = is_sunny and temprature > 20 and water_temprature 18
+cannot_go_outside = not is_sunny or temprature < 10 or wind_speed > 30
+
+# Don't delete the lines below
+print("Can go hiking:", can_go_hiking)
+print("Can go swimming:", can_go_swimming)
+print("Cannot go outside:", cannot_go_outside)
