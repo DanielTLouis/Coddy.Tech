ffc00cdf79cf189989d49c011ab532c0d2a66613
Daniel Louis <Llouis5314@gmail.com>
2025-10-29 20:03:12 -0400

    Create 5.2_Challangesolution.py

---
+++ Section_2/Chapter_05_AdvancedDecisionMaking/5.2_Challangesolution.py
@@ -0,0 +1,6 @@
+def check_inventory(products, quantities):
+    # Write code here
+    if 'Apples' in products: print("Apples are in stock.")
+    if not 'Oranges' in products: print("Oranges are not in stock.")
+    if 'Bananas' in quantities: print("Bananas quantity is tracked.")
+    if not 'Grapes' in quantities: print("Grapes quantity is not tracked.")
