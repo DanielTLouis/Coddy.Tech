8e2c7afc9873570705111ab636e1fa7fe84ce4d9
Daniel Louis <Llouis5314@gmail.com>
2025-11-09 19:31:04 -0500

    Create 6.2_Solution.py

---
+++ Section_2/Chapter_06_SetsPart1/6.2_Solution.py
@@ -0,0 +1,9 @@
+def manage_set(set1, element_to_add, element_to_remove):
+    # Write code here
+    set1.add(element_to_add)
+    if(element_to_remove in set1):
+        set1.remove(element_to_remove)
+    if(5 in set1):
+        return "5 is in the set"
+    else:
+        return "5 is not in the set"
