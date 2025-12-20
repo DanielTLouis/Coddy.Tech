ed4956e865e301d690e1e8322d156f15616eabeb
Daniel Louis <Llouis5314@gmail.com>
2025-12-19 22:19:57 -0500

    Create 9.2_Solution.py

---
+++ Section_2/Chapter_09_AdvancedDataAggregation/9.2_Solution.py
@@ -0,0 +1,8 @@
+numbers = [42, 17, 23, 56, 9, 34]
+words = ["kiwi", "apple", "banana", "cherry", "date"]
+
+# Write code here
+print("Smallest number:", min(numbers))
+print("Largest number:", max(numbers))
+print("Smallest word:", min(words))
+print("Largest word:", max(words))
