22cce83bd5bb88a0daf62b5d79488a1fec3d61f6
Daniel Louis <Llouis5314@gmail.com>
2026-01-11 09:53:21 -0500

    Create 11.1_Solution.py

---
+++ Section_2/Chapter_11_Advanced Functions/11.1_Solution.py
@@ -0,0 +1,10 @@
+# Write code here
+def get_student_info():
+    Name = "Bob"
+    Age = 20
+    Major = "Computer Science"
+    return Name, Age, Major 
+student_name, student_age, student_major = get_student_info()
+print(student_name)
+print(student_age)
+print(student_major) 
