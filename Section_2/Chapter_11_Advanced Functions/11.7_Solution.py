1b1dcbb891c7962199fa6579fef9c300cc1b1ac1
Daniel Louis <Llouis5314@gmail.com>
2026-01-17 20:07:47 -0500

    Update and rename 11.7_Testing.py to 11.7_Solution.py

--- Section_2/Chapter_11_Advanced Functions/11.7_Solution.py
+++ Section_2/Chapter_11_Advanced Functions/11.7_Solution.py
@@ -2,7 +2,7 @@ def sum_nested(nested_list):
     total = 0
     for i in nested_list:
         if(isinstance(i, list)):
-            sum_nested(i)
+            total += sum_nested(i)
         else:
             total += i
-    return total 
+    return total
