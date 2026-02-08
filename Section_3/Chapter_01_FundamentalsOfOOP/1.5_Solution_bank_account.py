5e0a698e49a29d00eb32ea4525738cf39b1a4d70
Daniel Louis <Llouis5314@gmail.com>
2026-02-07 21:15:44 -0500

    Create 1.5_Solution_bank_account.py

---
+++ Section_3/Chapter_01_FundamentalsOfOOP/1.5_Solution_bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    # TODO: Add class attribute here
+    bank_name = "Python National Bank"
+    
+    # TODO: Add the methods here
+    def deposit(self, amount):
+        self.balance += amount
+        pass
+    
+    def withdraw(self, amount):
+        self.balance -= amount
+        pass
+    
+    def get_balance(self):
+        return self.balance 
+        pass
