4b461fb4e7e6a3be182abec0309f3d2a10a738e1
Daniel Louis <Llouis5314@gmail.com>
2026-03-08 10:55:04 -0400

    Create 4.2_Challenge.py

---
+++ Section_3/Chapter_04_Inheritance/4.2_Challenge.py
@@ -0,0 +1,15 @@
+The super() Function
+
+
+
+Challenge
+
+Medium
+In this challenge, you'll implement a Person and Employee class hierarchy using inheritance.
+
+person.py: Contains the Person class with name and age attributes
+employee.py: Contains the Employee class that inherits from Person
+driver.py: Main file to test your implementation
+Each file contains detailed TODO comments to guide you through the implementation.
+
+
